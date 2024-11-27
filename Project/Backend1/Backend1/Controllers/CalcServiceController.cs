52c93242f5f4b52cad83c9f7aa17f92344528b1b
Ivan Lyutko <ivan.lyutko@bk.ru>
2024-11-27 21:58:12 +0700

    Добавлен PassUsingViewData

---
+++ Project/Backend1/Backend1/Controllers/CalcServiceController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend1.Controllers
+{
+    public class CalcServiceController : Controller
+    {
+        public IActionResult PassUsingViewData()
+        {
+            Random rnd = new Random();
+
+            int firstValue = rnd.Next(0, 11);
+            int secondValue = rnd.Next(0, 11);
+
+            ViewData["FirstValue"] = firstValue;
+            ViewData["SecondValue"] = secondValue;
+            ViewData["Add"] = firstValue + secondValue;
+            ViewData["Sub"] = firstValue - secondValue;
+            ViewData["Mult"] = firstValue * secondValue;
+
+            if(secondValue == 0)
+            {
+                ViewData["Div"] = "ERROR!";
+            }
+            else
+            {
+                ViewData["Div"] = (float)firstValue / secondValue;
+            }
+
+            return View();
+        }
+    }
+}
